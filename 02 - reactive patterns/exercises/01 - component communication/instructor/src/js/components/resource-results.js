edc6d0baade2c52eea45f6325b31590c0f072347
oaao <oliver+github@abolish.golf>
2026-01-29 16:36:21 -0700

    rev [02 - reactive patterns]: ex01 - Step 2: make a private results array & its setter

--- 02 - reactive patterns/exercises/01 - component communication/instructor/src/js/components/resource-results.js
+++ 02 - reactive patterns/exercises/01 - component communication/instructor/src/js/components/resource-results.js
@@ -21,6 +21,9 @@ template.innerHTML = `
 
 class ResourceResults extends HTMLElement {
   // TODO: Create a private field for results data
+  #results = [];  // # makes a property or method privately scoped.
+  // This means only methods/logic from the same instance (e.g. ResourceResults) can change it.
+  // We're going to have a controlled process for writing the data, and we don't want outside logic to be able to bypass that.
 
   constructor() {
     super();
@@ -30,6 +33,11 @@ class ResourceResults extends HTMLElement {
   }
 
   // TODO: Implement setter for results data, remember to render
+  set results(data) {
+    // The setter method for our private array - similar to what you're learning in C# right now.
+    this.#results = data;
+    this.render();  // when data is set, call our render method (below) to fire the display logic.
+  }
 
   // TODO: Add an event handler method for result selection
 
