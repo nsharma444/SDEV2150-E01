1bc712b1a8cc7c398463984a5b5a36c18ae1cbd1
oaao <oliver+github@abolish.golf>
2026-01-29 16:59:17 -0700

    rev [02 - reactive patterns]: ex02 - Step 2: skeleton & bindings, ResourceFilter events

--- 02 - reactive patterns/exercises/02 - reactive filtering/instructor/src/js/components/resource-filters.js
+++ 02 - reactive patterns/exercises/02 - reactive filtering/instructor/src/js/components/resource-filters.js
@@ -54,34 +54,55 @@ class ResourceFilters extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    // Bind method to instance -> see again: https://dev.to/aman_singh/why-do-we-need-to-bind-methods-inside-our-class-component-s-constructor-45bn
-    // Next time, we'll just use arrow functions ;)
     this._handleSubmit = this._handleSubmit.bind(this);
     this._handleCategoryClick = this._handleCategoryClick.bind(this);
   }
 
   connectedCallback() {
-    // If I want UI input to trigger events firing from this UI component,
-    // I'll need event listeners for those inputs.
+    // life cycle: We can't listen for events until the component loads into the DOM, so they go in here!
+    // This also means we need to render the component BEFORE attaching event listeners,
+    // Because .render() is what actually attaches the template content to the shadow root node first.
     this.render();
+
+    this._form = this.shadowRoot.querySelector('#frm-filter');
+    this._form.addEventListener('submit', this._handleSubmit);
+
+    this._categoryGroup = this.shadowRoot.querySelector('[aria-label="Category filters"]') // aria-label is its uniquely identifying attribute
+    this._categoryGroup.addEventListener('click', this._handleCategoryClick);
   }
 
   disconnectedCallback() {
-    // If I'm setting up event listeners when the component loads/attaches into the DOM (^ connectedCallback),
-    // I should clean up those event listeners if/when the component ever unloads (disconnectedCallback)
+    // life cycle: If you set up event listeners on load or (not in this example) on attribute change, clean 'em up!
+    if (this._form) {
+      this._form.removeEventListener('submit', this._handleSubmit);
+    }
+    if (this._categoryGroup) {
+      this._categoryGroup.removeEventListener('click', this._handleCategoryClick);
+    }
   }
 
    _handleCategoryClick(event) {
-    // - clicking categories (in this example, will trigger changes live, without clicking Filter)
+    // earlier comment was misleading, sorry!! we will just:
     //      - change click state of category button
   }
 
   _handleSubmit(event) {
-    event.preventDefault(); // If I'm handling form submission with JS, I already know I need to prevent default form submission behaviour
-    
-    // - submitting the filters to trigger an event and send a message
-    //      - build an object to hold the filter state
-    //      - create & fire a custom event that sends filter state
+    event.preventDefault();
+    // I'll need to collect data to pack into the filters object; let's get the skeleton out of the way first
+    const filters = {
+      // data elements go here, once I've extracted input values from the DOM
+    };
+
+    const filtersEvent = new CustomEvent(
+      'resource-filters-changed',
+      {
+        details: filters,
+        bubbles: true,
+        composed: true,
+      }
+    );
+
+    this.dispatchEvent(filtersEvent);
   }
 
   render() {
