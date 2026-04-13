6ed1d04834da1d21c57d44187d84453c31d61e86
Oliver Antoniu <oliver+github@abolish.golf>
2026-04-13 17:20:17 -0600

    refactor [09 - data fetching]: ex - translate declarative routing into data routing

--- 09 - data fetching/example - data fetching/instructor/frontend/src/main.jsx
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
 import App from './App.jsx';
 import ResourceDirectoryPage from './pages/ResourceDirectoryPage';
@@ -12,16 +8,22 @@ import AdminPage from './pages/AdminPage';
 
 import './index.css';
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: App,
+      children: [
+        {index: true, Component: ResourceDirectoryPage},
+        {path: "admin", Component: AdminPage},
+        {path: "admin/:resourceId", Component: AdminPage}
+      ]
+    }
+  ]
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<ResourceDirectoryPage />} />
-          <Route path="admin" element={<AdminPage />} />
-          <Route path="admin/:resourceId" element={<AdminPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
