2f58955fdcb7820e8499b6853951f8e4b4909494
Oliver Antoniu <oliver+github@abolish.golf>
2026-04-13 18:23:31 -0600

    rev [09 - data fetching]: ex - flesh out payload for AdminAction

--- 09 - data fetching/example - data fetching/instructor/frontend/src/router/actions.js
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/router/actions.js
@@ -7,9 +7,17 @@ export async function AdminAction({ request, params }) {
 
     const formData = await request.formData() // built-in via react-router
 
+    // there are cleaner ways to do this, but I'll just leave it explicit
     const payload = {
-        // skeleton for now, I'm going to digest from form data obj to build payload 
-    }
+      title: formData.get('title'),
+      category: formData.get('category'),
+      summary: formData.get('summary'),
+      location: formData.get('location'),
+      hours: formData.get('hours'),
+      contact: formData.get('contact'),
+      virtual: formData.get('virtual') === 'on', // checkbox is posted as 'on', not t/f
+      openNow: formData.get('openNow') === 'on',
+    };
 
     const savedResource = await saveResource(params.resourceId, payload)
     return redirect(`/admin/${savedResource.id}`);
