f5dac41823e0a1defc7a60c9b0c514a45b033d46
Oliver Antoniu <oliver+github@abolish.golf>
2026-04-20 18:37:04 -0600

    prj [10 - shared state]: ex02 - install and set up react-query layer

--- 10 - shared state/examples/02 - server-side/instructor/frontend/src/main.jsx
+++ 10 - shared state/examples/02 - server-side/instructor/frontend/src/main.jsx
@@ -5,8 +5,15 @@ import { RouterProvider } from 'react-router';
 import './index.css';
 import { router } from './router';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// initialise query client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
